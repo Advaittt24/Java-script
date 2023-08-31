060fbad9427be522cf6aa01fbe73aa75b3f83ac4
Advaittt24 <135209731+Advaittt24@users.noreply.github.com>
2023-08-31 10:25:41 +0000

    1st project complete

---
+++ function/funtion.js
@@ -0,0 +1,9 @@
+function add(){
+   let value = prompt("Enter Price = ")
+   let values = prompt("Enter GST = ")
+   value44 = Number(value)
+   value55 = Number(values)
+   let sum=((value44*value55)/100)+value44;
+   
+   alert("Total price is = "+sum)
+}
