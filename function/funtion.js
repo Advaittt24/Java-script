6c6686ed4aab75d976910bfe7c4fe0a4e827bcb7
Advaittt24 <135209731+Advaittt24@users.noreply.github.com>
2023-09-02 08:10:58 +0000

    updates

--- function/funtion.js
+++ function/funtion.js
@@ -7,3 +7,23 @@ function add(){
    
    alert("Total price is = "+sum)
 }
+
+function sub(){
+   let value3 = prompt("Enter A Number = ")
+   let value6 = prompt("Enter 2nd number = ")
+   value22 = Number(value3)
+   value33 = Number(value6)
+   let sum2 = (value22 - value33);
+
+   alert("Total = "+sum2)
+}
+
+function addt(){
+   let value9 = prompt("Enter A Number = ")
+   let value10 = prompt("Enter A Number = ")
+   value99 = Number(value9)
+   value100 = Number(value10)
+   let sum3 = (value99 + value100);
+
+   alert("Total = "+sum3)
+}
