00b3aae7560d45fb9cec682d27b74cb8f0c188da
Advaittt24 <135209731+Advaittt24@users.noreply.github.com>
2023-11-09 11:28:14 +0000

    little update

---
+++ functions /functions.js
@@ -0,0 +1,18 @@
+function add (a,b){
+    c= a+b
+    return c
+}
+c1=add(4.,5)
+console.log(c1)
+function sub (d,f){
+    g= d-f
+    return g
+}
+g1=sub(4,5)
+console.log(g1)
+function div (z,x){
+    q=z/x
+    return q
+}
+q1=div(4,5)
+console.log(q1,c1,g1)
