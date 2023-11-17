0685cd062b02799e4667b2d31edafc770228e14e
Advaittt24 <135209731+Advaittt24@users.noreply.github.com>
2023-11-17 11:27:21 +0000

    little update

---
+++ aaj ka/aaj.js
@@ -0,0 +1,15 @@
+let arr=[1,2,3,4,5,6,7,8,9,]
+// console.log(array)
+// for (let i = 0; i<array.length;i++){
+//     console.log(array[i])
+// }
+
+// a.forEach(function(element){
+//     console.log(element)
+
+// })
+let a = 0
+while (a < arr.length){
+    console.log(arr[a])
+    a++
+}
