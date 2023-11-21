d54c7172aad462e7593dd4fc5e93c30ba60972dc
Advaittt24 <135209731+Advaittt24@users.noreply.github.com>
2023-11-21 11:37:35 +0000

    dowhile loop done

--- loop/loop.js
+++ loop/loop.js
@@ -8,9 +8,18 @@
 //     console.log(element)
 
 // })
-let arr=[1,2,3,4,5,6,7,8,9,]
+// let arr=[1,2,3,4,5,6,7,8,9,]
+// let a = 0
+// while (a < arr.length){
+//     console.log(arr[a])
+//     a++
+// }
+let arr=[1,2,3,4,5,6,7,8,9]
 let a = 0
-while (a < arr.length){
+do{
     console.log(arr[a])
     a++
+}
+while(a<arr.length){
+
 }
