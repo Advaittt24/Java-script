f967e98a47af1113bfb207cbb42f9a2023b721d5
Advaittt24 <135209731+Advaittt24@users.noreply.github.com>
2023-11-23 11:32:25 +0000

    Array methods

---
+++ array method/index.js
@@ -0,0 +1,9 @@
+let myArr = ["Rahul","Aayush",2555,true,null,"Advait"]
+// array Methods
+// myArr.pop()
+// myArr.push("hehehe")
+// myArr.shift()
+myArr.reverse()
+// const newlen = myArr.unshift("Pratik")
+// console.log(newlen)
+console.log(myArr)
