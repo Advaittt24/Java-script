390050c5d9051e4bc21db02202847953b1292275
Advaittt24 <135209731+Advaittt24@users.noreply.github.com>
2023-11-23 17:38:21 +0000

    small update

--- array method/index.js
+++ array method/index.js
@@ -1,9 +1,19 @@
-let myArr = ["Rahul","Aayush",2555,true,null,"Advait"]
-// array Methods
-// myArr.pop()
-// myArr.push("hehehe")
-// myArr.shift()
-myArr.reverse()
-// const newlen = myArr.unshift("Pratik")
-// console.log(newlen)
-console.log(myArr)
+// let myArr = ["Rahul","Aayush",2555,true,null,"Advait"]
+// // array Methods
+// // myArr.pop()
+// // myArr.push("hehehe")
+// // myArr.shift()
+// myArr.reverse()
+// // const newlen = myArr.unshift("Pratik")
+// // console.log(newlen)
+// console.log(myArr)
+
+// string Method
+// let myStr = "Hello everyone"
+// console.log(myStr.length)
+// console.log(myStr.indexOf("Hello"))
+// console.log(mystr.lastIndexOf("everyone"))
+// console.log(myStr.slice(1,3))
+// d = myStr.replace("Hello","kjabvkhv")
+// d = d.replace("everone","oiqbc")
+// console.log(d,myStr)
